2e88421d6fbd5a00ba55ae2044cf6ddb488e285e
Abd-ELrahmanHamza <68310502+Abd-ELrahmanHamza@users.noreply.github.com>
2023-04-30 03:25:48 +0300

    add basic hooks

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 // Import react (for jsdoc)
 import React from "react";
 
-// Import Images
-import logo from "./logo.svg";
-
 // Import css
 import "./App.css";
 
-//
 import CssBaseline from "@mui/material/CssBaseline";
+import BasicButtons from "./Features/Common/Components/BasicButtons";
 
 /**
  * The main app of our application it handles routing
@@ -17,22 +14,9 @@ import CssBaseline from "@mui/material/CssBaseline";
  */
 function App() {
   return (
-    <div className="App">
+    <div>
       <CssBaseline />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BasicButtons />
     </div>
   );
 }
